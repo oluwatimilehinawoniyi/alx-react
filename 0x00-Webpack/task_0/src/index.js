c9ca5a5b3b2b40604093d3fcd0b77518c177ef02
oluwatimilehin awoniyi <awoniyitimi@outlook.com>
2024-04-02 11:42:24 +0100

    Basic setup

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,7 @@
+import $ from "jquery";
+
+const text1 = "<p>Holberton Dashboard</p>";
+const text2 = "<p>Dashboard data for the students</p>";
+const text3 = "<p>Copyright - Holberton School</p>";
+
+$("body").append(text1, text2, text3);
