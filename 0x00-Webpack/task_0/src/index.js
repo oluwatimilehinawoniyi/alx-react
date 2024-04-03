373ccce0eeccbd74e3ca86cb3ce87c0130c14b08
oluwatimilehin awoniyi <awoniyitimi@outlook.com>
2024-04-03 08:57:46 +0100

    basic setup

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,7 +1,5 @@
 import $ from "jquery";
 
-const text1 = "<p>Holberton Dashboard</p>";
-const text2 = "<p>Dashboard data for the students</p>";
-const text3 = "<p>Copyright - Holberton School</p>";
-
-$("body").append(text1, text2, text3);
+$("body").append("<p>Holberton Dashboard</p>");
+$("body").append("<p>Dashboard data for the students</p>");
+$("body").append("<p>Copyright - Holberton School</p>");
